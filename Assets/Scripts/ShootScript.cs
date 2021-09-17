77e3215909277d7127e96d3d2d0baac23edc4b33
Thái Hoàng Tuấn <tuanthai0403@gmail.com>
2021-09-17 22:57:09 +0700

    shooting

--- Assets/Scripts/ShootScript.cs
+++ Assets/Scripts/ShootScript.cs
@@ -7,24 +7,26 @@ public class ShootScript : MonoBehaviour
 
     public GameObject arCamera;
     public GameObject smoke;
-    Rigidbody2D rb;
-    void Start(){
-        rb = GetComponent<Rigidbody2D>();
-    }
+    // Rigidbody2D rb;
+    // void Start(){
+    //     rb = GetComponent<Rigidbody2D>();
+    // }
 
     public void Shoot() {
 
-        RaycastHit hit;
+        RaycastHit2D hit = Physics2D.Raycast(arCamera.transform.position, arCamera.transform.forward);
 
-        if (Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit)) {
+        if (hit.collider != null)
+        {
+             Debug.Log("Hit point" + hit.transform.name);
 
-            if (hit.transform.name == "Banana(Clone)" || hit.transform.name == "Banana(Clone)" || hit.transform.name == "Banana(Clone)" ) {
+             if (hit.transform.name == "Banana(Clone)") {
 
                 Destroy(hit.transform.gameObject);
 
                 Instantiate(smoke, hit.point, Quaternion.LookRotation(hit.normal));
             }
+           
         }
-
     }
 }
